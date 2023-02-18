12ee994063bfbe6a63893412dbf61bec9c532171
Jonathan Müller <jo-m@outlook.com>
2023-02-18 20:29:02 +0100

    clang-format

--- pkg/conv2/cconv.c
+++ pkg/conv2/cconv.c
@@ -1,65 +1,39 @@
 #include "cconv.h"
 
-void FullFillC(
-    int dy,
-    int dx,
-    int dv,
-    int du,
-    int n,
-    int m,
-    int fs,
-    int gs,
-    int outs,
-    float64 *fd,
-    float64 *gd,
-    float64 *outd
-) {
-    for (int y = 0; y < m; y++ ){
-		for (int x = 0; x < n; x++) {
-            float64 sum = 0;
+void FullFillC(int dy, int dx, int dv, int du, int n, int m, int fs, int gs, int outs, float64 *fd, float64 *gd,
+               float64 *outd) {
+  for (int y = 0; y < m; y++) {
+    for (int x = 0; x < n; x++) {
+      float64 sum = 0;
 
-			for (int v = 0; v < dv; v++) {
-				for (int u = 0; u < du; u++) {
-					if (y-v < 0 || y-v >= dy || x-u < 0 || x-u >= dx) {
-						continue; // zero
-					}
+      for (int v = 0; v < dv; v++) {
+        for (int u = 0; u < du; u++) {
+          if (y - v < 0 || y - v >= dy || x - u < 0 || x - u >= dx) {
+            continue;  // zero
+          }
 
-                    sum += fd[(y-v)*fs+(x-u)] * gd[v*gs+u];
-				}
-			}
+          sum += fd[(y - v) * fs + (x - u)] * gd[v * gs + u];
+        }
+      }
 
-            outd[y*outs+x] = sum;
-		}
-	}
+      outd[y * outs + x] = sum;
+    }
+  }
 }
 
+void ValidFillC(int dy, int dx, int dv, int du, int n, int m, int fs, int gs, int outs, float64 *fd, float64 *gd,
+                float64 *outd) {
+  for (int y = 0; y < m; y++) {
+    for (int x = 0; x < n; x++) {
+      float64 sum = 0;
 
-void ValidFillC(
-    int dy,
-    int dx,
-    int dv,
-    int du,
-    int n,
-    int m,
-    int fs,
-    int gs,
-    int outs,
-    float64 *fd,
-    float64 *gd,
-    float64 *outd
-) {
-    for (int y = 0; y < m; y++) {
-		for (int x = 0; x < n; x++) {
-			float64 sum = 0;
+      for (int v = 0; v < dv; v++) {
+        for (int u = 0; u < du; u++) {
+          sum += gd[v * gs + u] * fd[(y + dv - 1 - v) * fs + (x + du - 1 - u)];
+        }
+      }
 
-			for (int v = 0; v < dv; v++) {
-				for (int u = 0; u < du; u++) {
-					sum += gd[v*gs+u] * fd[(y+dv-1-v)*fs+(x+du-1-u)];
-				}
-			}
-
-            outd[y*outs+x] = sum;
-			// out.Set(y, x, sum)
-		}
-	}
+      outd[y * outs + x] = sum;
+    }
+  }
 }
--- pkg/conv2/cconv.h
+++ pkg/conv2/cconv.h
@@ -2,32 +2,8 @@
 
 #define float64 double
 
-void FullFillC(
-    int dy,
-    int dx,
-    int dv,
-    int du,
-    int n,
-    int m,
-    int fs,
-    int gs,
-    int outs,
-    float64 *fd,
-    float64 *gd,
-    float64 *outd
-);
+void FullFillC(int dy, int dx, int dv, int du, int n, int m, int fs, int gs, int outs, float64 *fd, float64 *gd,
+               float64 *outd);
 
-void ValidFillC(
-    int dy,
-    int dx,
-    int dv,
-    int du,
-    int n,
-    int m,
-    int fs,
-    int gs,
-    int outs,
-    float64 *fd,
-    float64 *gd,
-    float64 *outd
-);
+void ValidFillC(int dy, int dx, int dv, int du, int n, int m, int fs, int gs, int outs, float64 *fd, float64 *gd,
+                float64 *outd);
